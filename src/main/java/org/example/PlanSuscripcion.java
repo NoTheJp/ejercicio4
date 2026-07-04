8c72f4bc10a7b282ae272e51bcf2bba025ec6d1d
Jose Alberto Puentes M <josepm0703@gmail.com>
2026-07-04 12:52:12 -0600

    Interfaz & msg

---
+++ src/main/java/org/example/PlanSuscripcion.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface PlanSuscripcion {
+    double calcularCosto(int meses);
+
+        //hey, hice esta interfaz así rapidito para usarla
+        //o si quieres hacer tu propia interfaz/clase abstracta para los planes
+        //Yo me encargo de CuentaUsuario y PlataformaStreaming.
+        //Nómas avisame q show, yo al rato hago mi parte, sale?
+}
