632f06b8667d16f99721394bf599462d3b6dacb7
Jose Alberto Puentes M <josepm0703@gmail.com>
2026-07-04 12:52:30 -0600

    Interfaz & msg

--- src/main/java/org/example/PlanSuscripcion.java
+++ src/main/java/org/example/PlanSuscripcion.java
@@ -6,5 +6,5 @@ public interface PlanSuscripcion {
         //hey, hice esta interfaz así rapidito para usarla
         //o si quieres hacer tu propia interfaz/clase abstracta para los planes
         //Yo me encargo de CuentaUsuario y PlataformaStreaming.
-        //Nómas avisame q show, yo al rato hago mi parte, sale?
+        //Nómas avisame q show, yo al rato hago mi parte, sale??
 }
