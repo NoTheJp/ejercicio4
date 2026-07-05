857ad096e565c78af3edb8d9b36a09e32dbffea8
Jose Alberto Puentes M <josepm0703@gmail.com>
2026-07-05 16:53:28 -0600

    Crea CuentaUsuario: atributos y constructor

---
+++ src/main/java/org/example/CuentaUser.java
@@ -0,0 +1,15 @@
+package org.example;
+
+abstract class CuentaUser {
+    private String correo;
+    private int meses;
+    private PlanSuscripcion plan;
+
+    public CuentaUser (String Correo,  int Meses, PlanSuscripcion Plan){
+        this.correo = Correo;
+        this.meses = Meses;
+        this.plan = Plan;
+    }
+
+}
+
