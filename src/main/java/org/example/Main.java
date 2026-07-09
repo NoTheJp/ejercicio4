ec61ecd02e17df0b3c38d2e5c84bd36d5e5022c4
ken <kenneth.giacoman7@gmail.com>
2026-07-08 21:11:33 -0600

    plan basico y plan estandar en proceso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,13 @@
-package org.example;
-
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static Scanner teclado = new Scanner (System.in);
-    static void main() {
-        System.out.println("estoy en tu linea nebbercracker ");
-        String correo;
-        int meses;
-        int opc;
-        PlataformaStreaming reporte = new PlataformaStreaming();
-        do{
-            texto();
-            opc = teclado.nextInt();
-            switch (opc){
+public class Main{
 
-                case 1:
-                    System.out.println("Registre su usuario");
-                    correo = teclado.next();
-                    /* e we aquí te toca pedir los meses y hacer el switch de los 3 planes
-                    Luego, crea la cuenta y regístrala
-                    sale?
-                    */
-                break;
+   public static void main(String[] args){
 
-                case 2:
-                    reporte.reporte();
-                break;
+        Scanner sc = new Scanner(System.in);
 
-                case 0:
-                    System.out.println("Saliendo del sistema");
-                break;
+        PlataformaStreaming plataforma = new PlataformaStreaming();
 
-                default:
-                    System.out.println("pepito tenía un clavito");
-                break;
-            }
-        }while(opc != 0);
-    }
-    public static void texto(){
-        System.out.println("""
-                SELECCIONE UNA OPCION
-                1.- Registrar usuario
-                2.- Ver reporte
-                0.- Salir
-                """);
-        System.out.println("holaa");
-        System.out.println("te amo");
-    }
+        int opcion;
+   }
 }
--- src/main/java/org/example/PlataformaStreaming.java
+++ src/main/java/org/example/PlataformaStreaming.java
@@ -1,31 +1,30 @@
 package org.example;
-
 import java.util.ArrayList;
 
 public class PlataformaStreaming {
-    private ArrayList<CuentaUser> Corteos;
 
-    public PlataformaStreaming() {
-        this.Corteos = new ArrayList<>();
-    }
-
-    public void registrarUsuario(CuentaUser nuevo) {
-        this.Corteos.add(nuevo);
-    }
-
-    public void reporte(){
-
-        double gantotal = 0;
-        for (int i = 0; i < this.Corteos.size(); i++) {
-            CuentaUser nuevo = this.Corteos.get(i);
-            double totalcuenta = nuevo.obtenerTotalAPagar();
-
-            gantotal += totalcuenta;
-            System.out.println("Correo " + nuevo.getCorreo());
-            System.out.println("Meses " + nuevo.getMeses());
-            System.out.println("Plan " + nuevo.getPlan()); //creo q aqui van a salir cosas raras, así q ahí rifate un get o algo xfa
-            System.out.println("Total usuario " + totalcuenta);
+        private ArrayList<CuentaUsuario> cuentas;
+        public PlataformaStreaming(){
+            cuentas = new ArrayList<>();
         }
-        System.out.println("Total recaudado: " + gantotal);
-    }
+        public void registrarUsuario(CuentaUsuario cuenta){
+            cuentas.add(cuenta);
+        }
+
+        public void imprimirReporte(){
+            double total = 0;
+
+            System.out.println("reporte");
+            for(CuentaUsuario cuenta : cuentas){
+                double pago = cuenta.obtenerTotalApagar();
+
+                total += pago;
+
+                System.out.println("correo :" + cuenta.getCorreoElectronico());
+                System.out.println("plan   :" + cuenta.getPlan().getNombrePlan());
+                System.out.println("meses  :" + cuenta.getMesesActivo());
+                System.out.println("total  : $" + pago );
+            }
+            System.out.println("total recaudado : $" + total);
+        }
 }
