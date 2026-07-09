e6e3f759bdf8ad74066f02d4e861751f87634f61
ken <kenneth.giacoman7@gmail.com>
2026-07-08 22:04:18 -0600

    usuario streaming finalizado

--- src/main/java/org/example/UsuarioStreaming.java
+++ src/main/java/org/example/UsuarioStreaming.java
@@ -2,4 +2,8 @@ package org.example;
 
 public class UsuarioStreaming extends CuentaUsuario {
 
+    public UsuarioStreaming(String correoElectronico, int mesesActivo, PlanSuscripcion plan){
+        super(correoElectronico, mesesActivo, plan);
+    }
+
 }
